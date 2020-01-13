f8d075e2ace1f9b461102e0a22bf9e3f538e924e
xiaoran <tongxia1@msu.edu>
2020-01-13 06:45:03 -0500

    1) use readBSM as the basic reader for plink's Binary Symmetric Matrices; 2) treat # as comment char when reading id table accompanying a BSM; 3) change version number form 0.0.1 to 0.0.2

--- R/rel.R
+++ R/rel.R
@@ -1,155 +1,138 @@
-#' Read Binary Square Matrix
+#' read binary symmetric matrix
 #'
-#' Fetch square matrix and sample ID from a binary data and a
-#' two column text, respectively.
+#' read binary symmetric  matrix from PLINK files {p}.bin and  {p}.id, where {p}
+#' is a shared prefix.
+#'
+#' {p}.bin stores the matrix, which can be:
+#'
+#'   * the N x N symmetric matrix itself
+#'   * the lower triangle w/t diagonal
+#'   * the lower triangle w/o diagonal
 #' 
-#' For the binary matrix file, the function tries to figure
-#' out the storage type, since PLINK can stores a square
-#' matrix as:
+#' saved as either single or double precision.
 #'
-#' * N by N entries as it is
-#' * the lower triangle with the diagonal
-#' * the lower triangle without the diagonal
+#' {p}.id contains  family ID (FID)  and individual ID  (IID) in two  columns of
+#' text, by default only the 2nd column (IID) is used.
 #'
-#' Shape aside, PLINK chooses between single or double
-#' precision numbers, occupying 4 or 8 bytes per matrix entry,
-#' respectively.
+#' @param pfx shared prefix for two PLINK files
+#' @param dgn diagonal values for lower triangle without diagonal (def=1.0)
+#' @param fid read FID as well (def=FALSE)
+#' @param sep separator put between fid and iid to form the final ID
 #'
-#'
-#' As for sample ID, the text file has no header, and the two
-#' columns stand for family ID (FID) and the individual ID
-#' (IID) within family.
-#'
-#' For data exclusively composed of unrelated individuals, FID
-#' is set to be equal with IID in most cases. 
-#'
-#' Given the usual practice of assigning uniqe IID across the
-#' entire cohort despite some individuals forming families, the
-#' default behavior is fething IID only. If both FID also be
-#' requested, the final ID uses the form "{FID}.{IID}".
-#' 
-#' @param f.bin character filename of binary data
-#' @param f.id  character filename of sample IDs
-#' @param dg diagonal value for lower triangle without diagonal
-#' (def=1.0)
-#' @param iid.only TRUE to fetch individual ID only, ignore
-#' family ID (Def=TRUE)
-#'
-#' @return an N by N square matrix (GRM) load from file.
-.get.bm <- function(f.bin, f.id, dg=1, iid.only=TRUE)
+#' @return N by N symmetric matrix loaded from file.
+#' @export
+readBSM <- function(pfx, dgn=1, fid=FALSE, sep=".")
 {
-    ## -------------------- get sample IDs --------------------
-    id <- matrix(scan(f.id, "", quiet=TRUE), 2)
-    if(isTRUE(iid.only))
-        id <- id[2, ]
+    ## ID in {p}.id
+    ids <- matrix(scan(paste0(pfx, ".id"), quiet=TRUE, comment.char = "#"), 2)
+    if(isTRUE(fid))
+        ids <- paste(ids[1, ], ids[2, ], sep=sep)
     else
-        id <- paste(id[1, ], id[2, ], sep='.')
-    N <- length(id)
+        ids <- ids[1, ]
+    N <- length(ids)
     
-    
-    ## ------------------- get data matrix --------------------
-    S <- file.size(f.bin)               # file size
-    R <- matrix(.0, N, N, dimnames=list(id, id))
-    bin <- sub("[.].*$", "", basename(f.bin))
-    success <- FALSE
+    ## data matrix
+    bin <- paste0(pfx, ".bin")
+    S <- file.size(bin) # file size
+    R <- matrix(.0, N, N, dimnames=list(ids, ids))
+    H <- "UNK" # shape, start with unknown
 
-    ## try: lower triangle with diagonal
-    if(!success)
+    ## try: lower triangle w/t diagonal
+    if(H == "UNK")
     {
         L <- N * (N + 1.0) / 2.0
         U <- S / L
         if(U == 4 || U == 8)
         {
-            R[upper.tri(R, 1)] <- readBin(f.bin, .0, L, U)
+            R[upper.tri(R, 1)] <- readBin(bin, .0, L, U)
             R[lower.tri(R, 0)] <- t(R)[lower.tri(R, 0)]
-            success <- TRUE
+            H="LWD"
         }
     }
 
-    ## try: lower triangle without diagonal
-    if(!success)
+    ## try: lower triangle, no diagonal
+    if(H == "UNK")
     {
-        L <- N * (N - 1.0) / 2.0            # number of entries
-        U <- S / L                          # unit size
+        L <- N * (N - 1.0) / 2.0 # number of entries
+        U <- S / L               # unit size
         if(U == 4 || U == 8)
         {
-            R[upper.tri(R, 0)] <- readBin(f.bin, .0, L, U)
+            R[upper.tri(R, 0)] <- readBin(bin, .0, L, U)
             R[lower.tri(R, 0)] <- t(R)[lower.tri(R, 0)]
-            diag(R) <- dg                   # assigned diagnal
-            success <- TRUE
+            diag(R) <- dgn # diagnal
+            H="LND"
         }
     }
 
     ## try: a squre
-    if(!success)
+    if(H == "UNK")
     {
         L <- 1.0 * N * N
         U <- S / L
         if(U == 4 || U == 8)
         {
-            R[ , ] <- readBin(f.bin, .0, L, U)
-            success <- TRUE
+            R[ , ] <- readBin(bin, .0, L, U)
+            H="SQR"
         }
     }
 
     ## fail or return
-    print(data.frame(bin=bin, size=S, num.entries=L, unit.bytes=U, shape='LWD', N=N))
-    if(!success)
-        stop("can not figure out the shape of recoded entries in the relatedness matrix.")
+    print(data.frame(pfx=pfx, size=S, entries=L, unit=U, shape=H, N=N))
+    if(H == "UNK")
+        stop("unknown storage type.")
     R
 }
 
-#' Infer Sample ID from a Relatedness Matrix
+#' Infer Sample ID from a symmetric matrix
 #'
-#' Exam the row name a relatedness matrix for family IDs and
-#' individual IDs, which form the sample IDs.
+#' Exam the row name family and individual id.
 #'
-#' The IDs are automatically generate for symmetric matrices
-#' without a row name.
+#' For matrices without rowname, id are automatically generated.
 #' 
 #' By common practice, the row names or a relatedness matrix are
 #' in the form of [FID.]IID. Samples without family ID are given
 #' one identical to their individual ID.
 #'
-#' @param rmx relatedness matrix
+#' @param x  matrix
 #' 
 #' @return data.frame of inferred family ID and individual ID.
-.inf.id <- function(rmx)
+.inf.id <- function(x, sep=".")
 {
-    N <- nrow(rmx)
-    i <- row.names(rmx)
+    N <- nrow(x)
+    i <- row.names(x)
     if(is.null(i))
         i <- sprintf('I%d05', seq(N))
 
-    i <- lapply(strsplit(i, '[.]'), rep, l=2)
+    sep <- paste0('[', sep, ']')
+    i <- lapply(strsplit(i, sep), rep, l=2)
     i <- data.frame(do.call(rbind, i), stringsAsFactors=FALSE)
     names(i) <- c('FID', 'IID')
     i
 }
 
-#' Write Squre Matrix to PLINK Binary
+#' Save Symmetric Matrix to Binary
 #'
-#' Both the matrix data and subject ID will be written to
-#' files with a given prefix.
+#' save the matrix to binary {p}.bin, and  the row names text file {p}.id, where
+#' {p} is a given file prefix.
 #' 
-#' @param f character prefix of target files
+#' @param pfx prefix of output files
 #' @param x matrix to be saved
-#' @param l logical only store use lower triangle? def=TRUE
-#' @param d logical store diagnal? def=TRUE
-#' @param u numerical unit size, def=4 (single precision)
-.put.bm <- function(f, x, l=TRUE, d=TRUE, u=4L)
+#' @param l store the lower triangle? def=TRUE
+#' @param d store the diagnal? def=TRUE
+#' @param u numerical unit, def=4 (single precision)
+#' @export
+saveBSM <- function(pfx, x, l=TRUE, d=TRUE, u=4L)
 {
     ## save subject IDs
     id <- .inf.id(x)
-    write(t(id), paste0(f, '.id'), 2L, sep='\t')
+    write(t(id), paste0(pfx, '.id'), 2L, sep='\t')
     
-    ## R is colume major, to save the lower TRI, one extract
-    ## upper TRI instead.
+    ## R is colume majored, to save the lower TRI, use upper TRI instead.
     if(isTRUE(l))
         x <- x[upper.tri(x, d)]
         
     ## save matrix data
-    writeBin(x, paste0(f, '.bin'), u)
+    writeBin(x, paste0(pfx, '.bin'), u)
 }
 
 
@@ -157,33 +140,49 @@
 #'
 #' A PLINK IBS (Identity by State) matrix is represented by
 #'
-#' * .mibs.bin   : the binary file for N by N relatedness matrix;
-#' * .mibs.id    : list of samples IDs
+#'   * {p}.mibs.bin   : IBS matrix in binary
+#'   * {p}.mibs.id    : samples FID and IID in text
+#'
+#' IBS matrix is a result of PLINK --distance ibs
 #'
 #' @param pfx shared prefix of the three files
 #' @return matrix of relatedness
 #' @export
-readIBS <- function(pfx)
-{
-    .get.bm(paste0(pfx, ".mibs.bin"), paste0(pfx, ".mibs.id"))
-}
+readIBS <- function(pfx) readBSM(paste0(pfx, ".mibs"))
 
 #' Read PLINK Binary REL matrix
 #'
-#' PLINK REL (relatedness) matrix comes with two files:
+#' A PLINK REL (Relatedness) matrix is represented by
 #'
-#' * .grm.bin : an N by N matrix of relatedness;
-#' * .grm.id  : a list of sample ID.
+#'   * {p}.rel.bin   : IBS matrix in binary
+#'   * {p}.rel.id    : samples FID and IID in text
+#'
+#' REL matrix is a result of PLINK --distance ibs
 #'
 #' @param pfx shared prefix of the three files
 #' @return a relatedness matrix, with row and column names set to subject IDs.
 #' @export
-readREL <- function(pfx)
-{
-    .get.bm(paste0(pfx, ".rel.bin"), paste0(pfx, ".rel.id"))
-}
+readREL <- function(pfx) readBSM(paste0(pfx, ".rel"))
 
-saveREL <- function(pfx, grm)
+#' Read GRM binary of GCTA
+#'
+#' GRM (genetic relatedness matrix) is the main formt of GCTA, basically a PLINK
+#' REL with differnent surfix:
+#'
+#'   * .grm.bin : binary matrix,
+#'   * .grm.id  : samples,
+#'
+#' and it always uses single precision.
+#' 
+#' GRM  also comes  with another  binary matrix  ".grm.N.bin" counting  variants
+#' contributed to each relatedness.
+#'
+#' @param pfx shared prefix of data files
+#' @return N by N matrix of relatedness
+#' @export
+readGRM <- function(pfx) readBSM(paste0(pfx, ".grm"))
+
+saveGRM <- function(pfx, grm)
 {
     ## get file names
     fn.rmx <- paste0(pfx, ".grm.bin")
@@ -213,30 +212,6 @@ saveREL <- function(pfx, grm)
         write(t(id), fn.id, 2, sep='\t')
     })
 }
-
-
-#' Read GRM binary of GCTA
-#'
-#' GRM (genetic relatedness matrix) is the main formt of GCTA,
-#' developed by Jian Yang et. al. A GRM is basically a PLINK
-#' REL matrix with differnent surfix:
-#'
-#' * .grm.bin : binary relatedness matrix;
-#' * .grm.id  : list of samples.
-#'
-#' A GRM comes with an addtional binary matrix ".grm.N.bin" that
-#' counts the genomic variants contributed to each relatedness,
-#' also is always use single precision numbers, and stores lower
-#' triangle with diagnonal. These specifics however.
-#'
-#' @param pfx shared prefix of data files
-#' @return N by N matrix of relatedness
-#' @export
-readGRM <- function(pfx)
-{
-    .get.bm(paste0(pfx, ".grm.bin"), paste0(pfx, ".grm.id"))
-}
-
 
 #' Test Genetic Relatedness Matrix Reader
 #'
